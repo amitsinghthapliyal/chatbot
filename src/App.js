8924d975458b3860a0c55acfd7ed50b0176fa351
amit.s <amit.s@trainee.innouner.com>
2023-07-26 09:00:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import DraggableContainer from "./components/DraggableContainer";
-import Hello from "./components/Hello";
 
 function App() {
   return (
     <div>
-      <Hello />
       <DraggableContainer />
     </div>
   );
